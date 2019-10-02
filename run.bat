7476de59406f4561ec4bb43595b79422eb104dc7
Quin <cheesits456@gmail.com>
2019-10-02 02:07:50 -0700

    modified run.bat
    removed the need for setup.bat by including installers in run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,17 @@
 @echo off
-:a
+
+if exist node_modules/discord.js (
+	goto main
+) else (
+	goto init
+)
+
+:init
+echo Installing required files...
+call npm i discord.js
+goto main
+
+:main
 node ./bot.js
 timeout /t 5 /nobreak>nul
-goto a
+goto main
