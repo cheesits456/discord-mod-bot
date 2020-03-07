7ef34fe7e60499ca790edf13361d07e11534730e
solonovamax <solonovamax@12oclockpoint.com>
2020-03-06 22:50:09 -0500

    Added run.sh for linux users.

---
+++ run.sh
@@ -0,0 +1,11 @@
+if [ ! -e "node_modules/discord.js" ]; then
+	echo "Installing required files..."
+	npm i discord.js
+fi
+
+while [ true ]
+do
+	echo "Starting bot..."
+	node ./bot.js
+	sleep 2
+done
